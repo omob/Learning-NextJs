cf0e77e5b1d5067ae7c5fc79321160e690875a1e
Ayodeji Abodunrin <aboayosam@gmail.com>
2021-03-13 11:13:34 +0100

    learning' dynamic routing

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
 import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -20,7 +22,12 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>
+          I am a Software Engineer with proficiency in both front-end and
+          back-end technologies. I have developed Web Applications using the
+          MERN (MongoDB, Express, React.js, and Node.js) and MEAN (MongoDB,
+          Express, Angular, and Node.js) technology stack.
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
@@ -29,18 +36,19 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData?.map(({ id, date, title}) => (
+          {allPostsData?.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
       </section>
-     
     </Layout>
   );
 }
